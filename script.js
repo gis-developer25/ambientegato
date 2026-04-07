cdca9644ac7c62071258bbf02b1dcfa7c336497c
Giselle Gomes <giselle.emcristo@gmail.com>
2026-04-07 07:00:43 -0300

    Primeiro comit do projetogatos

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Página carregada com sucesso!");
+
+const botao = document.querySelector(".cta a");
+
+botao.addEventListener("click", () => {
+  alert("Obrigado por apoiar a causa animal!");
+});
